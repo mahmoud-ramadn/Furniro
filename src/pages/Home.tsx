8eff6b89cb57e35a50fde9664feee8dba56d84cd
mahmoud-ramadn <mr9026573@gmail.com>
2025-01-04 23:23:54 +0200

    add skeleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,6 +73,7 @@ function Home() {
             );
           })}
         </div>
+        
       </section>
 
       <OurProducts />
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,10 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../layouts/Mainlayout/MainLayout";
 import Error from "../pages/Error";
-import Home from "../pages/Home";
+import Hero from "../components/skeleteon/Hero";
+const Home =lazy(()=>import('../pages/Home'));
+
 
 const router = createBrowserRouter([
   {
@@ -32,11 +34,7 @@ const router = createBrowserRouter([
         element: (
           <Suspense
             fallback={
-              <div className=" text-green-500  flex justify-center items-center w-full  min-h-screen">
-                <h1 className=" text-4xl font-bold  text-violet-400">
-                  Loading .....
-                </h1>
-              </div>
+              <Hero/>
             }
           >
             <Home />
